47504a327a1a0fac17c6f6b63966df17c1e310dd
itishprasad30 <itishprasad20@gmail.com>
2022-02-24 20:29:29 +0530

    error fixed

--- pages/person/[id].js
+++ pages/person/[id].js
@@ -19,7 +19,7 @@ const Personid = () => {
     fetcher
   );
   if (error) return <div>not found</div>;
-  if (!data) return <div>Loding...</div>;
+  if (!data) return <div>Loading...</div>;
 
   // getting the date hours to adjusting the greetings
   let [first, last, opt] = data.name.split(" ");
@@ -41,7 +41,25 @@ const Personid = () => {
     <div className="bg-gray-400 mx-auto   h-screen space-x-3 flex flex-col  items-center mb-32">
       {/* Nice little Greetings For the User to come in */}
       <div className="p-2 w-full md:max-w-3xl ">
-        {welcomeText === "Good morning " || "Good afternoon" ? (
+        {welcomeText === "Good evening" ? (
+          <div className="overflow-hidden bg-gradient-to-l from-gray-900 via-white to-white rounded-lg shadow">
+            <div className="px-4 py-5  sm:p-6 flex justify-between items-center">
+              <h1 className="text-4xl font-bold">
+                {welcomeText}, {first}.
+              </h1>
+              <span className="text-white">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-12 w-12"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                </svg>
+              </span>
+            </div>
+          </div>
+        ) : (
           <div className="overflow-hidden bg-gradient-to-l from-blue-600 via-white to-white rounded-lg shadow">
             <div className="px-4 py-5  sm:p-6  flex justify-between items-center">
               <h1 className="text-4xl font-bold">
@@ -59,24 +77,6 @@ const Personid = () => {
                     d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
                     clipRule="evenodd"
                   ></path>
-                </svg>
-              </span>
-            </div>
-          </div>
-        ) : (
-          <div className="overflow-hidden bg-gradient-to-l from-gray-900 via-white to-white rounded-lg shadow">
-            <div className="px-4 py-5  sm:p-6 flex justify-between items-center">
-              <h1 className="text-4xl font-bold">
-                {welcomeText}, {first}.
-              </h1>
-              <span className="text-white">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-12 w-12"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
                 </svg>
               </span>
             </div>
